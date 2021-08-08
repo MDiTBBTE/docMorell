6d2c406d478ae998b7a4fa5df1b9dedff678f8cd
Andrii Korniienko <akorniienko@softjourn.com>
2021-08-09 00:04:46 +0300

    articles

--- client/pages/artikel/index.tsx
+++ client/pages/artikel/index.tsx
@@ -3,8 +3,12 @@ import Layout from "../../layouts/Layout";
 import { Breadcrumbs } from "../../components/Breadcrumbs/Breadcrumbs";
 import { NextThunkDispatch, wrapper } from "../../store";
 import { fetchCategories } from "../../store/actions-creators/category";
+import { fetchArticles } from "../../store/actions-creators/article";
+import { useTypedSelector } from "../../hooks/useTypedSelector";
 
 const Artikel = () => {
+  const { articles, articleError } = useTypedSelector((state) => state.article);
+
   return (
     <Layout>
       <div className="container">
@@ -20,5 +24,6 @@ export const getServerSideProps = wrapper.getServerSideProps(
   async ({ store }) => {
     const dispatch = store.dispatch as NextThunkDispatch;
     await dispatch(await fetchCategories());
+    await dispatch(await fetchArticles());
   }
 );
